dc63df0ceff73682e0e18b8f233316ccdbb0630b
Roman <tynchirov.r@gmail.com>
2023-08-16 16:26:54 +0300

    Исправлено #2

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Controller
 public class HelloController {
 
-    CarService carService = new CarServiceImpl();
+   private final CarService carService = new CarServiceImpl();
 
     @GetMapping(value = "/")
     public String printWelcome(ModelMap model) {
